bb5089262cb44e9925dbe9ca0b6564e7598edc86
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-24 16:14:00 +0000

    Add feature test for rating functionality

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'reviewing' do
+  before {Restaurant.create name: 'Ovens'}
+
+  scenario 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review Ovens'
+    fill_in "Thoughts", with: "awsome"
+    select '4', from: 'Raiting'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('awsome')
+  end
+end
