b3b7efa50781a03cef749ff61039db39978b3f67
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-24 14:35:15 +0000

    Allow to create new restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,19 +5,30 @@ feature 'restaurants:' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_link 'Add restaurant'
     end
   end
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'Nandos')
+      Restaurant.create(name: 'Ovens')
     end
 
     scenario 'display restaurants' do
       visit '/restaurants'
-      expect(page).to have_content('Nandos')
+      expect(page).to have_content('Ovens')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add restaurant'
+      fill_in 'Name', with: 'Ovens'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Ovens'
+      expect(current_path).to eq '/restaurants'
     end
   end
 
