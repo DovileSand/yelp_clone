4878bc6b15c4a00ab27b800ae18d326bec4e2a1e
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-24 15:53:50 +0000

    Shows, Edits and Deletes Restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -32,4 +32,36 @@ feature 'restaurants:' do
     end
   end
 
+  context 'viewing restaurants' do
+    let!(:ovens){Restaurant.create(name:'Ovens')}
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'Ovens'
+      expect(page).to have_content 'Ovens'
+      expect(current_path).to eq "/restaurants/#{ovens.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+    before{Restaurant.create name: 'Ovens'}
+    scenario 'lets a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit Ovens'
+      fill_in 'Name', with: "Best Aubergine Bakes"
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Best Aubergine Bakes'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+    before{Restaurant.create name: 'Ovens'}
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete Ovens'
+      expect(page).to have_content 'Ovens'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
 end
