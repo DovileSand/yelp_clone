6e764eac39dcd18130517a3f0f9104a58fb7f9d4
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-28 21:13:10 +0000

    Calculates average rating and allows to see Endorse Review button

---
+++ spec/features/endorsements_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'endorsing reviews' do
+  before do
+    ovens = Restaurant.create(name: 'Ovens')
+    ovens.reviews.create(rating: 3, thoughts: 'Let me think...')
+  end
+
+  scenario 'a user can endore a review, which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link 'Endorse Review'
+    expect(page).to have_content('1 endorsement')
+  end
+end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,14 +4,16 @@ context 'reviewing:' do
   before {Restaurant.create name: 'Ovens'}
 
   scenario 'allows users to leave a review using a form' do
-    visit '/restaurants'
-    click_link 'Review Ovens'
-    fill_in "Thoughts", with: "awesome"
-    select '4', from: 'Rating'
-    click_button 'Leave Review'
-
+    leave_review('awesome', '4')
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('awesome')
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    visit('/restaurants')
+    expect(page).to have_content('Average rating: 4')
   end
 
   # scenario 'cannot delete review that user did not create' do
